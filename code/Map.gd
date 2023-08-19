6c82b3a285f250a52120c5f6086664f1487b945f
Jagginator <142578953+Jagginator@users.noreply.github.com>
2023-08-19 16:42:29 +0200

    Map script added
    Map code

---
+++ code/Map.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+
+# Place enemy scenes in here
+ 
+#func _on_left_path_pressed():
+	#get_tree().change_scene_to_file()
+
+
+#func _on_middle_path_pressed():
+	#get_tree().change_scene_to_file()
+
+
+#func _on_right_path_pressed():
+	#get_tree().change_scene_to_file()
